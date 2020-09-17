375af9a4fb5e83e4a10ee28f932c2dad6e06dd67
Mykola Diachok <ndyachok@gmail.com>
2020-09-17 22:13:00 +0300

    added swagger

--- WebBoxOffice/Startup.cs
+++ WebBoxOffice/Startup.cs
@@ -11,6 +11,7 @@ using WebBoxOffice.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace WebBoxOffice
 {
@@ -26,6 +27,10 @@ namespace WebBoxOffice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebBoxOffice API", Version = "v1" });
+            });
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -50,6 +55,11 @@ namespace WebBoxOffice
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebBoxOffice API v1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
