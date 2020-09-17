740b247e0db23ff074b5bc3daa033e1a1d62c471
Mykola Diachok <ndyachok@gmail.com>
2020-09-17 21:52:39 +0300

    added serilog and version info

---
+++ WebBoxOffice/Core/Extensions/Version.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebBoxOffice.Core.Extensions
+{
+    public static class Version
+    {
+        /// <summary>
+        /// GetVersion
+        /// </summary>
+        /// <returns></returns>
+        public static string GetVersion() => $"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.Commits}-{ThisAssembly.Git.Branch}+{ThisAssembly.Git.Commit}";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAppVersion() => typeof(Program)
+            .GetTypeInfo()
+            .Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion;
+    }
+}
